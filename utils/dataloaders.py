339fa36c229944f1410d04da42e40f09fce67f23
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-07 19:48:55 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -491,12 +491,11 @@ class LoadImagesAndLabels(Dataset):
         cache_path = (p if p.is_file() else Path(self.label_files[0]).parent).with_suffix('.cache')
         # self.label_files = np.load('../datasets/custom/labels/train/train_Y.npy')
         # self.im_files = np.load('../datasets/custom/images/train/train_X.npy')
-        print(p)
-        print(cache_path)
-        exit()
         try:
-            # cache_path = '../datasets/custom/images/train/'
+            cache_path = '../datasets/custom/images/train.cache'
             cache, exists = np.load(cache_path, allow_pickle=True).item(), True  # load dict
+            print(cache)
+            exit()
             assert cache['version'] == self.cache_version  # matches current version
             assert cache['hash'] == get_hash(self.label_files + self.im_files)  # identical hash
         except Exception:
