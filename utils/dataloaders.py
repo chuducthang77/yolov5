57ff783b03f801c1227bcdbedc067f69d3a4872f
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-07 22:58:33 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1018,7 +1018,7 @@ def verify_image_label(args):
         else:
             im = im_file.reshape(64, 64, 3)
             shape = 64, 64
-
+        print(shape)
         exit()
         assert (shape[0] > 9) & (shape[1] > 9), f'image size {shape} <10 pixels'
         assert im.format.lower() in IMG_FORMATS, f'invalid image format {im.format}'
