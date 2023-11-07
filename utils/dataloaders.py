e05b6e24ecca276f514afefddfab8429b0b11076
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-07 23:11:40 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1029,13 +1029,29 @@ def verify_image_label(args):
         # verify labels
         if type(lb_file) != str:
             nf = 1
+            lb = np.array(lb_file, dtype=np.float32)
+            #Ignore the segment for the moment
+            nl = len(lb)
+            if nl:
+                assert lb.shape[1] == 5, f'labels require 5 columns, {lb.shape[1]} columns detected'
+                assert (lb >= 0).all(), f'negative label values {lb[lb < 0]}'
+                assert (lb[:, 1:] <= 1).all(), f'non-normalized or out of bounds coordinates {lb[:, 1:][lb[:, 1:] > 1]}'
+                _, i = np.unique(lb, axis=0, return_index=True)
+                if len(i) < nl:  # duplicate row check
+                    lb = lb[i]  # remove duplicates
+                    msg = f'{prefix}WARNING ⚠️ {im_file}: {nl - len(i)} duplicate labels removed'
+                else:
+                    ne = 1  # label empty
+                    lb = np.zeros((0, 5), dtype=np.float32)
+            else:
+                nm = 1  # label missing
+                lb = np.zeros((0, 5), dtype=np.float32)
+            return im_file, lb, shape, segments, nm, nf, ne, nc, msg
 
         if os.path.isfile(lb_file):
             nf = 1  # label found
             with open(lb_file) as f:
                 lb = [x.split() for x in f.read().strip().splitlines() if len(x)]
-                print(lb)
-                exit()
                 if any(len(x) > 6 for x in lb):  # is segment
                     classes = np.array([x[0] for x in lb], dtype=np.float32)
                     segments = [np.array(x[1:], dtype=np.float32).reshape(-1, 2) for x in lb]  # (cls, xy1...)
