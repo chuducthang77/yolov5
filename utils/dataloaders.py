c8933da6ddc60538cb173a453d1dce445106cffa
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-08 10:25:16 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -528,7 +528,7 @@ class LoadImagesAndLabels(Dataset):
         self.im_files = list(cache.keys())  # update
         self.label_files = img2label_paths(cache.keys())  # update
 
-        print(len(self.shape))
+        print(len(self.shapes))
         print('Before: ')
         # Filter images
         if min_items:
