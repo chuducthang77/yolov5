ca7b211cfcc9abff5021b7d0bddc0f948dcee58b
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-10 09:34:34 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -757,8 +757,6 @@ class LoadImagesAndLabels(Dataset):
     def load_image(self, i):
         # Loads 1 image from dataset index 'i', returns (im, original hw, resized hw)
         im, f, fn = self.ims[i], self.im_files[i], self.npy_files[i],
-        print(im)
-        exit()
         if im is None:  # not cached in RAM
             if fn.exists():  # load npy
                 im = np.load(fn)
