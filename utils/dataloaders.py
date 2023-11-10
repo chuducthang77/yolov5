1b3a4a6b90ebc76b1b3b1caff41a1c6048b4669b
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-10 09:26:05 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -757,17 +757,16 @@ class LoadImagesAndLabels(Dataset):
     def load_image(self, i):
         # Loads 1 image from dataset index 'i', returns (im, original hw, resized hw)
         im, f, fn = self.ims[i], self.im_files[i], self.npy_files[i],
-        print(fn)
-        exit()
         if im is None:  # not cached in RAM
             if type(fn) != str:
                 im = fn
             elif fn.exists():  # load npy
                 im = np.load(fn)
+                print('Here')
             else:  # read image
                 im = cv2.imread(f)  # BGR
                 assert im is not None, f'Image Not Found {f}'
-            print(im.shape)
+            # print(im.shape)
             print(im)
             exit()
             h0, w0 = im.shape[:2]  # orig hw
