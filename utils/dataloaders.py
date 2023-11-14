801683f4a4549b9743382ed03820d4fb65315729
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-14 22:53:46 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -499,6 +499,9 @@ class LoadImagesAndLabels(Dataset):
                 self.label_files = list(np.load('../datasets/custom-seg/labels/train/train_Y.npy', allow_pickle=True))
                 print(self.label_files)
                 print('self label files')
+                print(len(self.label_files))
+                print(self.label_files.shape)
+                print(self.labels_files[0, 0].shape)
                 exit()
             else:
                 self.label_files = list(np.load('../datasets/custom/labels/train/train_Y.npy'))
