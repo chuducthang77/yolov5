c11ff4a0e9bea8f25b85c8f4ce1fec4e03c96ddf
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-14 22:17:26 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -464,6 +464,8 @@ class LoadImagesAndLabels(Dataset):
         self.stride = stride
         self.path = path
         self.albumentations = Albumentations(size=img_size) if augment else None
+        print('Helloooooo good morning')
+        exit()
         try:
             f = []  # image files
             for p in path if isinstance(path, list) else [path]:
