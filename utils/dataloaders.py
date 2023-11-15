dd9da09fa6dedb29cd5d86ad8a0d757885ef2989
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-15 09:26:07 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -643,6 +643,8 @@ class LoadImagesAndLabels(Dataset):
         nm, nf, ne, nc, msgs = 0, 0, 0, 0, []  # number missing, found, empty, corrupt, messages
         desc = f'{prefix}Scanning {path.parent / path.stem}...'
         # print(self.label_files)
+        print('Hello again!')
+        exit()
         with Pool(NUM_THREADS) as pool:
             pbar = tqdm(pool.imap(verify_image_label, zip(self.im_files, self.label_files, repeat(prefix))),
                         desc=desc,
@@ -1074,8 +1076,6 @@ def verify_image_label(args):
 
         else:
             if os.path.isfile(lb_file):
-                print('Hellooo i am here')
-                exit()
                 nf = 1  # label found
                 with open(lb_file) as f:
                     lb = [x.split() for x in f.read().strip().splitlines() if len(x)]
