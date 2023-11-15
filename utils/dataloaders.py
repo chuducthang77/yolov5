9ac623188adc89dc7479ed9002108bf5c708fbf7
Chu Đức Thắng <chuducthang77@gmail.com>
2023-11-15 09:35:30 +0700

    Temp change

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1053,12 +1053,12 @@ def verify_image_label(args):
         #             ImageOps.exif_transpose(Image.open(im_file)).save(im_file, 'JPEG', subsampling=0, quality=100)
         #             msg = f'{prefix}WARNING ⚠️ {im_file}: corrupt JPEG restored and saved'
         # verify labels
+        print(lb_file)
+        exit()
         if type(lb_file) != str:
             nf = 1
             lb = np.array(lb_file, dtype=np.float32)
             #Ignore the segment for the moment
-            print('Numpy file here')
-            exit()
             nl = len(lb)
             if nl:
                 # assert lb.shape[1] == 5, f'labels require 5 columns, {lb.shape[1]} columns detected'
